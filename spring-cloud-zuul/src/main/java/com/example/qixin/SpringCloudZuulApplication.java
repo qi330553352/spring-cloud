4061ec65e448504b71ed36e26b0f64d35022e5c5
qi330553352 <35446537+qi330553352@users.noreply.github.com>
2018-06-07 22:37:24 +0800

    测试1

--- spring-cloud-zuul/src/main/java/com/example/qixin/SpringCloudZuulApplication.java
+++ spring-cloud-zuul/src/main/java/com/example/qixin/SpringCloudZuulApplication.java
@@ -12,6 +12,8 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 public class SpringCloudZuulApplication {
 
 	public static void main(String[] args) {
+
+
 		SpringApplication.run(SpringCloudZuulApplication.class, args);
 	}
 }
