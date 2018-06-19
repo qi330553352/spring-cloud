c06c1170386faf6e70d653eadb7d6fb7ed952e50
qi330553352 <35446537+qi330553352@users.noreply.github.com>
2018-06-19 08:41:04 +0800

    OAuth2.0授权（客户端）。。。。。。。。。。。。。。。

--- base-client-authorize-plugins/src/main/java/com/example/qixin/BaseClientAuthorizePlugins.java
+++ base-client-authorize-plugins/src/main/java/com/example/qixin/BaseClientAuthorizePlugins.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /** https://blog.csdn.net/tianyaleixiaowu/article/details/78281392
  *  http://developer.baidu.com/wiki/index.php?title=docs/oauth/showcase
+ *  https://blog.csdn.net/u014386474/article/details/51602264
+ *  https://github.com/zhouyongtao/homeinns-web
  * 创  建   时  间： 2018/6/18 15:34
  * 版           本: V1.0
  * 作           者: qixin
