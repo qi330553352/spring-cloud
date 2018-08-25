7f2c5382f4def0d8d9bccfa0e499d79b091a8ed4
sxxyadmin/cs_qixin <Qixin123>
2018-08-25 10:11:28 +0800

    初始化.........

---
+++ base-file-plugins/base-file-api/src/main/java/com/example/qixin/BaseFileApi.java
@@ -0,0 +1,13 @@
+package com.example.qixin;
+
+/**
+ * 创 建  时 间： 2018/8/25 10:06
+ * 版           本:  V1.0
+ * 作           者:  qixin
+ */
+public class BaseFileApi {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ base-file-plugins/base-file-server/src/test/java/com/example/qixin/base/file/server/BaseFileServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.qixin.base.file.server;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BaseFileServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
