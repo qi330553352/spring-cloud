7c24ebe99a5f78c8f034ec628a6aa46a0d9ee1ff
qi330553352 <35446537+qi330553352@users.noreply.github.com>
2018-08-30 01:01:10 +0800

    oauth2.0

--- base-oauth2.0-plugins/oauth2.0-server-plugins/src/main/java/com/example/qixin/SecurityConfig.java
+++ base-oauth2.0-plugins/oauth2.0-server-plugins/src/main/java/com/example/qixin/SecurityConfig.java
@@ -1,9 +1,13 @@
 package com.example.qixin;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -17,40 +21,29 @@ import javax.sql.DataSource;
  */
 @Configuration
 @EnableWebSecurity  //开启安全认证
+@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private DataSource dataSource;
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.jdbcAuthentication().dataSource(dataSource);
+    @Override //基于数据库表进行认证 (配置user-detial服务)
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+
+        //auth.jdbcAuthentication().dataSource(dataSource);
+        auth.inMemoryAuthentication()
+                .withUser("user").password("123").roles("USER")
+                .and()
+                .withUser("admin").password("123").roles("ADMIN");
     }
 
-
-    @Override // 配置默认登陆页面，登陆失败页面
+    @Override // 配置默认登陆页面，登陆失败页面 （配置Spring Security的Filter链）
     protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
         http.authorizeRequests()
-                .anyRequest().authenticated()
-                .and()
-                .csrf().disable() //关闭CSRF
-                .formLogin()
-                .loginPage("/login")
-                .failureUrl("/oauth/authorize")
-                .permitAll()
-                .and()
-                .logout().permitAll()
+                .antMatchers("/oauth/authorize").authenticated()
                 .and()
                 .httpBasic().realmName("OAuth Server");
-
     }
 
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http.csrf().disable();
-//        http.authorizeRequests()
-//                .antMatchers("/oauth/authorize").authenticated()
-//                .and()
-//                .httpBasic().realmName("OAuth Server");
-//    }
 }
