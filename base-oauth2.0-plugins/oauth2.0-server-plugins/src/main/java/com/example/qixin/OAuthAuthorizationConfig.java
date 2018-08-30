9d4cca87542704099194d995a6a8bdb7bf6fd083
qi330553352 <35446537+qi330553352@users.noreply.github.com>
2018-08-31 01:19:55 +0800

    oauth2.0

--- base-oauth2.0-plugins/oauth2.0-server-plugins/src/main/java/com/example/qixin/OAuthAuthorizationConfig.java
+++ base-oauth2.0-plugins/oauth2.0-server-plugins/src/main/java/com/example/qixin/OAuthAuthorizationConfig.java
@@ -1,9 +1,25 @@
 package com.example.qixin;
 
+import com.example.qixin.configuration.CustomJdbcAuthorizationCodeServices;
+import com.example.qixin.configuration.CustomTokenEnhancer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.approval.ApprovalStore;
+import org.springframework.security.oauth2.provider.approval.JdbcApprovalStore;
+import org.springframework.security.oauth2.provider.code.AuthorizationCodeServices;
+import org.springframework.security.oauth2.provider.code.JdbcAuthorizationCodeServices;
+import org.springframework.security.oauth2.provider.endpoint.AuthorizationEndpoint;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+
+import javax.annotation.PostConstruct;
+import javax.sql.DataSource;
 
 /**
  * 创  建   时  间： 2018/8/22 22:08
@@ -15,17 +31,40 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 @EnableAuthorizationServer  //配置授权服务
 public class OAuthAuthorizationConfig extends AuthorizationServerConfigurerAdapter {
 
+    @Autowired
+    private DataSource dataSource;
+
+    @Bean
+    public ApprovalStore approvalStore() {
+
+        return new JdbcApprovalStore(dataSource);
+    }
+    @Bean
+    protected AuthorizationCodeServices authorizationCodeServices() {
+
+        return new JdbcAuthorizationCodeServices(dataSource);
+    }
+    @Bean
+    public TokenStore tokenStore() {
+
+        return new JdbcTokenStore(dataSource);
+    }
+
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-        clients.inMemory()
-                .withClient(Config.OAUTH_CLIENT_ID)
-                .secret(Config.OAUTH_CLIENT_SECRET)
-                .resourceIds(Config.RESOURCE_ID)
-                .scopes(Config.SCOPES)
-                .authorities("ROLE_USER")
-                .authorizedGrantTypes("authorization_code", "refresh_token")
-                .redirectUris("http://default-oauth-callback.com")
-                .accessTokenValiditySeconds(60*30) // 30min
-                .refreshTokenValiditySeconds(60*60*24); // 24h
+
+        clients.jdbc(dataSource); // oauth_client_details
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints.approvalStore(approvalStore())                            // oauth_approvals
+                .authorizationCodeServices(authorizationCodeServices())    // oauth_code
+                .tokenStore(tokenStore());                                 // oauth_access_token & oauth_refresh_token
+    }
+    @Override //解决Spring Security OAuth在访问/oauth/token时候报401 authentication is required
+    public void configure(AuthorizationServerSecurityConfigurer oauthServer)throws Exception {
+
+        oauthServer.allowFormAuthenticationForClients();
     }
 }
