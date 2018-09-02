475522da3b666ad5bbb29d434ec4af5b11bc3690
qi330553352 <35446537+qi330553352@users.noreply.github.com>
2018-09-02 14:12:58 +0800

    oauth2.0

--- base-oauth2.0-plugins/oauth2.0-resource-plugins/src/main/java/com/example/qixin/ProfileController.java
+++ base-oauth2.0-plugins/oauth2.0-resource-plugins/src/main/java/com/example/qixin/ProfileController.java
@@ -1,10 +1,15 @@
 package com.example.qixin;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+//@Log4j2
 @RestController
 public class ProfileController {
+
+	private Logger log = LoggerFactory.getLogger(this.getClass());
 
     @GetMapping("/api/profile")
     public Profile get() {
@@ -13,6 +18,7 @@ public class ProfileController {
     	p.setName("rensanning");
     	p.setEmail("rensanning@gmail.com");
     	p.setRemake("此为资源服务器数据");
+		log.info("返回对象为：{}",p);
         return p;
     }
 
