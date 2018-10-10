d86626f98c8df852fa1ffec2d41a5886cda4f34e
qi330553352 <35446537+qi330553352@users.noreply.github.com>
2018-10-10 20:51:46 +0800

    提交代码

---
+++ base-file-plugins/base-file-api/src/main/java/com/example/qixin/entity/BaseFile.java
@@ -0,0 +1,19 @@
+package com.example.qixin.entity;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+
+/**
+ * 创  建   时  间： 2018/10/7 23:06
+ * 版           本: V1.0
+ * 作           者: qixin
+ * 版  权   所  有: 版权所有(C)2016-2026
+ */
+@Data
+public class BaseFile {
+
+    @Id
+    private String id;
+
+
+}
