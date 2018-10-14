dfa19355405c05cea7fc45be8082bb8a2329eac8
qi330553352 <35446537+qi330553352@users.noreply.github.com>
2018-10-14 14:52:14 +0800

    提交代码

--- base-file-plugins/base-file-server/src/main/java/com/example/qixin/BaseFileServer.java
+++ base-file-plugins/base-file-server/src/main/java/com/example/qixin/BaseFileServer.java
@@ -12,6 +12,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BaseFileServer {
 
     public static void main(String[] args) {
+
         SpringApplication.run(BaseFileServer.class, args);
     }
 }
