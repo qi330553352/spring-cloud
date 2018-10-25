53104ec73d0d884b8bb42b461ce005e679dd037e
qi330553352 <35446537+qi330553352@users.noreply.github.com>
2018-10-26 00:26:43 +0800

    提交

--- aggregation-client/src/main/java/com/example/qixin/AggregationClient.java
+++ aggregation-client/src/main/java/com/example/qixin/AggregationClient.java
@@ -14,6 +14,7 @@ import org.springframework.context.annotation.Bean;
  * 作           者: qixin
  * 版  权   所  有: 版权所有(C)2016-2026
  */
+@EnableEurekaClient
 @EnableCircuitBreaker //开启服务降级熔断器
 @SpringBootApplication
 public class AggregationClient {
--- aggregation-client/src/main/java/com/example/qixin/controller/RestTestController.java
+++ aggregation-client/src/main/java/com/example/qixin/controller/RestTestController.java
@@ -1,7 +1,11 @@
 package com.example.qixin.controller;
 
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -12,11 +16,29 @@ import org.springframework.web.client.RestTemplate;
  * 版  权   所  有: 版权所有(C)2016-2026
  */
 @Log4j2
+@RefreshScope
 @RestController
 public class RestTestController {
 
+    @Value("${configuration.name}")
+    private String name;
+
+    @RequestMapping("/config")
+    public String config(){
+
+        return this.name;
+    }
+
     @GetMapping("/test")
     public String test() {
+        RestTemplate restTemplate=new RestTemplate() ;
+        String str = restTemplate.getForObject("https://www.baidu.com/",String.class);
+        log.info("str:{}",str);
+        return str;
+    }
+
+    @GetMapping("/test1")
+    public String test1() {
         RestTemplate restTemplate=new RestTemplate() ;
         String str = restTemplate.getForObject("https://www.baidu.com/",String.class);
         log.info("str:{}",str);
