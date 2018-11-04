023bc3197ce551120f7ccf641bd1e71ad7e4a5e8
qi330553352 <35446537+qi330553352@users.noreply.github.com>
2018-11-04 22:00:41 +0800

    abc

--- base-info-plugins/base-info-server/src/main/java/com/example/qixin/BaseInfoServer.java
+++ base-info-plugins/base-info-server/src/main/java/com/example/qixin/BaseInfoServer.java
@@ -1,14 +1,14 @@
 package com.example.qixin;
 
-import com.example.qixin.stream.SendService;
+import com.example.qixin.stream.IReceiveService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 
 @EnableDiscoveryClient
-@EnableBinding({SendService.class})
 @SpringBootApplication
+@EnableBinding({IReceiveService.class})
 public class BaseInfoServer {
 
 	public static void main(String[] args) {
---
+++ base-info-plugins/base-info-server/src/main/java/com/example/qixin/stream/IReceiveService.java
@@ -0,0 +1,18 @@
+package com.example.qixin.stream;
+
+import org.springframework.cloud.stream.annotation.Input;
+import org.springframework.messaging.SubscribableChannel;
+
+/**
+ * 创  建   时  间： 2018/11/3 11:07
+ * 版           本: V1.0
+ * 作           者: qixin
+ * 版  权   所  有: 版权所有(C)2016-2026
+ */
+public interface IReceiveService {
+
+    String INPUT = "input";
+
+    @Input(INPUT)
+    SubscribableChannel receive();
+}
