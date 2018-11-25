987f8a72fd2c33576f6e01ffb6fe26f26bde06d9
qi330553352 <35446537+qi330553352@users.noreply.github.com>
2018-11-25 11:07:29 +0800

    eureka 2.0

---
+++ chapter-eureka/eureka-provider/src/main/java/com/example/qixin/config/WebSecurityConfig.java
@@ -0,0 +1,19 @@
+//package com.example.qixin.config;
+//
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * 创  建   时  间： 2018/11/25 0:09
+ * 版           本: V1.0
+ * 作           者: qixin
+ * 版  权   所  有: 版权所有(C)2016-2026
+ */
+//@EnableWebSecurity
+//public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.csrf().disable();
+//    }
+//}
