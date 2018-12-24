6d372ca198dea20e29b8456d5a69c1d031d55ca9
qi330553352 <35446537+qi330553352@users.noreply.github.com>
2018-12-25 00:08:48 +0800

    132

--- chapter-config/config-client/src/main/java/com/example/qixin/ConfigClientApplication.java
+++ chapter-config/config-client/src/main/java/com/example/qixin/ConfigClientApplication.java
@@ -3,18 +3,12 @@ package com.example.qixin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.config.server.EnableConfigServer;
 
-/**
- * http://127.0.0.1:9030/application/default
- */
-@EnableConfigServer
+@EnableDiscoveryClient
 @SpringBootApplication
-public class ConfigServerApplication {
+public class ConfigClientApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ConfigServerApplication.class, args);
+		SpringApplication.run(ConfigClientApplication.class, args);
 	}
-
 }
-
