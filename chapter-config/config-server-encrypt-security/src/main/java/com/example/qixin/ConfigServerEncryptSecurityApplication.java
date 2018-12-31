c3ce1a58b0bc125a0e6d78709f8899c3226ac792
qi330553352 <35446537+qi330553352@users.noreply.github.com>
2018-12-31 18:59:30 +0800

    132

--- chapter-config/config-server-encrypt-security/src/main/java/com/example/qixin/ConfigServerEncryptSecurityApplication.java
+++ chapter-config/config-server-encrypt-security/src/main/java/com/example/qixin/ConfigServerEncryptSecurityApplication.java
@@ -3,16 +3,19 @@ package com.example.qixin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
- * http://127.0.0.1:9030/application/default
+ * http://127.0.0.1:9030/encrypt/status
+ * http://127.0.0.1:9030/encrypt-symmetric/default
  */
 @EnableConfigServer
+@EnableEurekaClient
 @SpringBootApplication
-public class ConfigServerApplication {
+public class ConfigServerEncryptSecurityApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ConfigServerApplication.class, args);
+		SpringApplication.run(ConfigServerEncryptSecurityApplication.class, args);
 	}
 
 }
